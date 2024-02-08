781fd4780b19312a4ad05e6f9adc3a486ca9a300
Aaniq Allauddin <aaniq.allauddin@gmail.com>
2024-02-08 06:54:31 +0500

    task4 (updated) version

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -1,55 +1,30 @@
 package com.college.converter;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-/*
-    TODOs:
-    In groups of 4, complete the following tasks, 1 for each team member:
-    1. Extract all the strings into the strings.xml file and use them in the layout and the activity
-    2. Change the theme of the app to a NoActionBar theme and modify the primary colors
-    3. Add Log messages at the entry/exit of onCreate() and convertCurrency methods. Level should be Info
-    4. Add ViewBinding to the project
-
-    ** Each task must be done in a separate branch and merged to the main branch
-    after completion using a Pull Request.
-    ** Each task must be done by a different team member.
-
-*/
+import com.college.converter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     static private final Float CONVERSION_RATE = 0.80F;
+    private ActivityMainBinding binding; // Declare binding object
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); // Inflate layout using View Binding
+        setContentView(binding.getRoot().getRootView()); // Set the root view of the layout as the content view
 
-        Button buttonConvert = findViewById(R.id.convertButton);
-
-        buttonConvert.setOnClickListener( view ->  {
-            convertCurrency(view);
-        } );
+        // Set onClickListener using method reference
+        binding.convertButton.setOnClickListener(this::convertCurrency);
     }
-
     public void convertCurrency(View view) {
-
-        EditText inputView = findViewById(R.id.entryId);
-
-        String inputAmount = inputView.getText().toString();
-
-        TextView resultView = findViewById(R.id.resultId);
-
+        String inputAmount = binding.entryId.getText().toString(); // Access EditText using View Binding
         if (!inputAmount.isEmpty()) {
             Float inputAmountDecimal = Float.valueOf(inputAmount);
-
             Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
-
-            resultView.setText( resultFloat + " Euros" );
+            binding.resultId.setText(resultFloat + " Euros"); // Access TextView using View Binding
         }
     }
 }
